bd593fbe4607aab4a95eec9e77f5e610d868b6d1
Urs Lang <urs.lang@gmail.com>
2022-08-30 08:13:45 +0200

    .

--- src/Base/AbstractDbDataSource.php
+++ src/Base/AbstractDbDataSource.php
@@ -1,1 +1,1 @@
-<?phpnamespace Nemundo\Db\Base;use Nemundo\Core\Base\DataSource\AbstractDataSource;abstract class AbstractDbDataSource extends AbstractDataSource{    use ConnectionTrait;    public function __construct()    {        $this->loadConnection();    }}
+<?phpnamespace Nemundo\Db\Base;use Nemundo\Core\Base\DataSource\AbstractDataSource;abstract class AbstractDbDataSource extends AbstractDataSource{    use ConnectionTrait;    public function __construct()    {        parent::__construct();        $this->loadConnection();    }}
